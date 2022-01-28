badae31f83470c10ccaab16d4345064cafa85adf
LBeul <l.beul@outlook.de>
2022-01-28 11:25:12 +0100

    1.7 unicafe step2

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -4,20 +4,30 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [avg, setAvg] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   const onGoodClick = event => {
     event.preventDefault()
     setGood(good + 1)
+    adjustOverallStats()
   }
 
   const onNeutralClick = event => {
     event.preventDefault()
     setNeutral(neutral + 1)
+    adjustOverallStats()
   }
 
   const onBadClick = event => {
     event.preventDefault()
     setBad(bad + 1)
+    adjustOverallStats()
+  }
+
+  const adjustOverallStats = () => {
+    setAll(all + 1)
   }
 
   return (
@@ -27,7 +37,12 @@ const App = () => {
         onNeutralClick={onNeutralClick}
         onBadClick={onBadClick}
       />
-      <Statistics goodClicks={good} neutralClicks={neutral} badClicks={bad} />
+      <Statistics
+        goodClicks={good}
+        neutralClicks={neutral}
+        badClicks={bad}
+        allClicks={all}
+      />
     </>
   )
 }
@@ -47,12 +62,20 @@ const FeedbackButtons = ({ onGoodClick, onNeutralClick, onBadClick }) => (
   </>
 )
 
-const Statistics = ({ goodClicks, neutralClicks, badClicks }) => (
-  <>
-    <p>good: {goodClicks}</p>
-    <p>neutral: {neutralClicks}</p>
-    <p>bad: {badClicks}</p>
-  </>
-)
+const Statistics = ({ goodClicks, neutralClicks, badClicks, allClicks }) => {
+  const positive = allClicks != 0 ? goodClicks / allClicks : 0
+  const average = allClicks != 0 ? (goodClicks - badClicks) / allClicks : 0
+
+  return (
+    <>
+      <p>good: {goodClicks}</p>
+      <p>neutral: {neutralClicks}</p>
+      <p>bad: {badClicks}</p>
+      <p>all: {allClicks}</p>
+      <p>average: {average}</p>
+      <p>positive: {positive} %</p>
+    </>
+  )
+}
 
 export default App
