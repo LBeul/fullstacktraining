61c07292d268e6509bb34d7ab51270378088cbda
LBeul <l.beul@outlook.de>
2023-01-13 12:26:17 +0100

    relate users and posts to each other

--- backend/notes/controllers/notes.js
+++ backend/notes/controllers/notes.js
@@ -1,5 +1,6 @@
 const notesRouter = require('express').Router();
 const Note = require('../models/note');
+const User = require('../models/user');
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note.find({});
@@ -16,15 +17,20 @@ notesRouter.get('/:id', async (request, response) => {
 });
 
 notesRouter.post('/', async (request, response) => {
-  const body = request.body;
+  const { content, important, userId } = request.body;
+  const user = await User.findById(userId);
 
   const note = new Note({
-    content: body.content,
-    important: body.important || false,
+    user: user._id,
+    content: content,
+    important: important || false,
     date: new Date(),
   });
 
   const savedNote = await note.save();
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
+
   response.status(201).json(savedNote);
 });
 
