8be58da4d5789d56197432acb01921f5fb34f14c
LBeul <l.beul@outlook.de>
2023-01-19 14:46:14 +0100

    use morgan for logging

--- backend/blogList/app.js
+++ backend/blogList/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import morgan from 'morgan';
 import 'dotenv/config';
 
 import blogsRouter from './controllers/blogs.js';
+import usersRouter from './controllers/users.js';
 
 const app = express();
 
@@ -17,8 +19,10 @@ mongoose
 // Invoke middleware
 app.use(cors());
 app.use(express.json());
+app.use(morgan('tiny'));
 
 // Invoke routers
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 
 export default app;
