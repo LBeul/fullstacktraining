39e92314820364f19c3543eb7cf99be7c4d4603f
LBeul <l.beul@outlook.de>
2023-01-19 14:46:38 +0100

    enable user endpoints & passweord hashing

---
+++ backend/blogList/controllers/users.js
@@ -0,0 +1,33 @@
+import { Router } from 'express';
+import bcrypt from 'bcrypt';
+import User from '../models/user.js';
+
+const usersRouter = Router();
+
+usersRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body;
+
+  const existingUser = await User.findOne({ username });
+  if (existingUser) {
+    return response.status(400).json('username is already taken');
+  }
+  if (password.length < 10) {
+    return response
+      .status(400)
+      .json('password has to be 10 characters or longer');
+  }
+
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  const user = new User({ username, name, passwordHash });
+  const savedUser = await user.save();
+
+  response.status(201).json(savedUser);
+});
+
+usersRouter.get('/', (request, response) => {
+  User.find({}).then((users) => response.json(users));
+});
+
+export default usersRouter;
